4da1be1a333eaf7d2ae175978b6191ee576fab6d
David MASSENOT <david.massenot@doctena.com>
2023-06-14 10:00:17 +0200

    Usage controller & route

---
+++ src/Controller/Application/UsageController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller\Application;
+
+use App\Service\OpenAIService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * @author David
+ */
+class UsageController extends AbstractController
+{
+
+    protected OpenAIService $openAIService;
+
+    /**
+     * Constructor
+     *
+     * @param OpenAIService $openAIService
+     */
+    public function __construct(OpenAIService $openAIService) {
+        $this->openAIService = $openAIService;
+    }
+
+    /**
+     * @Route("/usage", name="application_usage_index", methods={"GET"})
+     *
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render(
+            'application/usage/index.html.twig',
+            []
+        );
+    }
+}
