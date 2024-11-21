054a85a8fee5ae158c8726aeceeee5a8e6d153cd
David Massenot <david.massenot@doctena.com>
2024-11-21 07:35:33 +0100

    Symfony Live component installation

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\LiveComponent\LiveComponentBundle::class => ['all' => true],
 ];
